eca79fba7e3ee286c4edd944c883bfb93b7d6fd8
unknown <sparsh262002@gmail.com>
2024-07-20 11:52:08 +0530

    Login

---
+++ frontend/src/pages/Login.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import LoginBox from '../components/LoginBox'
+
+export default function Login() {
+  return (
+    <>
+    <LoginBox/>
+    </>
+  )
+}
---
+++ frontend/src/pages/SignUp.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import SignUpBox from '../components/SignUpBox'
+
+export default function SignUp() {
+  return (
+    <div className='mt-[50px]'>
+      <SignUpBox/>
+    </div>
+  )
+}
